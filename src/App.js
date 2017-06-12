e44ab7023c3171e7a074c164027d8a5d2db1b1fb
Michael Villis <mvillis@gmail.com>
2017-06-12 23:56:53 +1000

    added first blog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBlock,
+  CardTitle, CardSubtitle, Button, Container, Row, Col } from 'reactstrap';
 import { Timeline } from 'react-twitter-widgets'
 import './App.css';
+import rocket from './spacex-71873.jpg';
 
 class App extends Component {
   render() {
@@ -15,24 +18,55 @@ class App extends Component {
           </div>
           <h2>Mike Villis</h2>
         </div>
-        <p className="App-intro">
-          Coming soon.
-        </p>
-        <div className='Twitter'>
-          <Timeline
-            dataSource={{
-              sourceType: 'profile',
-              screenName: 'mvillis'
-            }}
-            options={{
-              username: 'mvillis',
-              height: '500'
-            }}
-            onLoad={() => console.log('Timeline is loaded!')}
-          />
+        <Container>
+          <Row>
+            <Col><h3>Blogs</h3></Col>
+          </Row>
+          <Row>
+            <Col><hr /></Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={4}>
+              <Card>
+                <CardImg top width="100%" src={rocket} alt="Rocket" />
+                <CardBlock>
+                  <CardTitle>First Blog</CardTitle>
+                  <CardSubtitle>Let&apos;s go!</CardSubtitle>
+                  <CardText>I&apos;ve been sitting on mikevillis.com for almost 12 months now. At the time of...</CardText>
+                  <Button href='https://medium.com/bikebytes/launch-55583c4df741'>Read More</Button>
+                </CardBlock>
+              </Card>
+            </Col>
+            <Col xs={12} md={4}></Col>
+            <Col xs={12} md={4}></Col>
+            <Col xs={12} md={4}></Col>
+          </Row>
+          <Row className='margin'>
+            <Col><h3>Tweets</h3></Col>
+          </Row>
+          <Row>
+            <Col><hr /></Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className='Twitter'>
+                <Timeline
+                  dataSource={{
+                    sourceType: 'profile',
+                    screenName: 'mvillis'
+                  }}
+                  options={{
+                    username: 'mvillis',
+                    height: '500'
+                  }}
+                  onLoad={() => console.log('Timeline is loaded!')}
+                />
+            </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
-      </div>
-    );
+    )
   }
 }
 
